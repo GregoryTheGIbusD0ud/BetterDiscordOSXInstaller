36f0336aa047d7906c112759fa4f15a3e42b7fbf
den <den@denbuntu.home>
2017-02-04 15:27:24 +0000

    Initial Upload, v1.0

---
+++ main.js
@@ -0,0 +1,214 @@
+const electron = require('electron')
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+const path = require('path')
+const url = require('url')
+const ipc = require('electron').ipcMain;
+const request = require('request');
+const fs = require('fs');
+const unzip = require('unzip');
+const readline = require('readline');
+
+const asar = require('./asar');
+
+// npm install readline, fs, request, url, child_process, unzip
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+var fileurl = 'https://github.com/Jiiks/BetterDiscordApp/archive/stable16.zip';
+var discordpath = "/Applications/Discord.app/Contents/Resources/";
+var a = new asar(discordpath+'app.asar');
+var discordexec = "/Applications/Discord.app"
+let mainWindow
+
+
+
+
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    title:"BetterDiscord OSX Installer by den",
+    resizable: false,icon: __dirname + '/images/betterdiscord_icon.png',
+    width: 800, height: 600}
+    );
+
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  app.quit();
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) { createWindow(); }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IPC Pipeline between main.js and index.html
+
+
+
+ipc.on('openWebsite', function (event) {
+  //exec('open https://betterdiscord.net');
+  exec('open https://betterdiscord.net')
+});
+
+
+
+
+ipc.on('closeDiscord', function (event,data){
+  exec('killall -SIGKILL Discord');
+  event.sender.send('discordClosed',true);
+});
+
+
+ipc.on('appChecker', function (event, data){
+  if (fs.existsSync(discordpath+'app')) {
+    try {
+      (execSync('rm -rf '+discordpath+'app'))
+      event.sender.send('appChecked', true);
+      console.log('deleted app folder');
+    } catch (err) {
+      console.log('failed to delete app file');
+      event.sender.send('appChecked', false);
+    }
+  } else {
+    event.sender.send('appChecked', true);
+      console.log('No app folder found');
+  }
+});
+ipc.on('startAsar', function (event, data){
+    setTimeout(function() { appAsarExtract(event) }, 1500);
+});
+
+ipc.on('downloadZip', function (event){
+    setTimeout(function() { downloadZipfile(fileurl, event)}, 1500);
+});
+
+ipc.on('injectJS', function (event){
+    setTimeout(function() {injectBD(event)}, 1500);
+});
+
+ipc.on('relaunchDiscord', function (event){
+    setTimeout(function() {relaunchDiscord(event)}, 1500);
+    console.log('relaunching discord')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions used by installer
+
+
+
+
+
+
+function appAsarExtract(event){
+  a.extract(msg => { 
+    console.log(msg);
+  },
+  (curIndex, total) => {
+  }, err => {
+      if(err === null) {
+        console.log("Extraction complete")
+        event.sender.send('asarComplete', true);
+      } else {
+        console.log(err)
+        event.sender.send('asarComplete', false);
+      }
+  });
+}
+
+
+function downloadZipfile(fileurl, event){
+  request(fileurl)
+  .pipe(fs.createWriteStream('/tmp/stable16.zip'))
+  .on('close', function () {
+    console.log('BetterDiscord modules saved to /tmp/stable16.zip');
+    fs.createReadStream('/tmp/stable16.zip').pipe(unzip.Extract({ path: discordpath+"app/node_modules/" }).on('close', function(){
+      console.log('moving to node_modules and renaming to betterdiscord')
+      fs.rename(discordpath+"app/node_modules/BetterDiscordApp-stable16", discordpath+"app/node_modules/betterdiscord", function (err) {
+        if (err) {
+          throw err;
+          console.log('Error thrown during file move');
+          event.sender.send('zipComplete', false);
+        } else {
+          console.log('renamed BetterDiscordApp-stable16 to betterdiscord');
+          event.sender.send('zipComplete', true);
+        }
+      });
+    }));
+  });
+}
+
+function injectBD(event){
+		var lines = [];
+		var linereader = readline.createInterface({input: fs.createReadStream(discordpath+'app/index.js')});
+    try {
+      linereader.on('line', line => {
+        lines.push(line);
+        if(line.indexOf("'use strict';") > -1) { 
+          lines.push("var _betterDiscord = require('betterdiscord');");
+          lines.push("var _betterDiscord2;");
+        }
+
+        if(line.indexOf("mainWindow = new BrowserWindow(mainWindowOptions);") > -1) { 
+          lines.push('    _betterDiscord2 = new _betterDiscord.BetterDiscord(mainWindow, false);');
+        }
+      });
+      linereader.on('close', () => {
+        fs.writeFileSync(discordpath+'app/index.js', lines.join('\n'));
+        event.sender.send('injectComplete',true);
+      });
+    } catch (err){
+      console.log("Error injecting BetterDiscord Code.")
+      event.sender.send('injectComplete',false);
+    }
+}
+
+function relaunchDiscord(event){
+  try {
+    console.log('relaunching discord');
+    exec('open '+discordexec);
+    console.log('discord launched');
+    event.sender.send('relaunchComplete',true);
+  } catch (err) {
+    console.log('error relaunching discord');
+    console.log(err);
+    event.sender.send('relaunchComplete',false);
+  }
+}
